41d09528bd45a6fba6c096c6e384c44c9ff05cde
daddygang <gyao96@qq.com>
2020-04-17 19:12:14 -0700

    first commit

---
+++ Assets/Scripts/DrawDebugCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DrawDebugCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    cam = GetComponent<Camera>();
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(this.transform.position, this.transform.position + transform.forward * 3f, Color.green);
+    }
+}
---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+
+    public enum ShapeType { Sphere, Cube, Torus, RCube };
+    public enum Operation { None, Blend, Cut, Mask }
+
+    public ShapeType shapeType;
+    public Operation operation;
+    public Color colour = Color.white;
+    public float radius;
+    [Range(0, 1)]
+    public float blendStrength;
+    public float movespeed = 1f;
+    public Vector3 movedir;
+    public float moveradius = 0.5f;
+    [HideInInspector]
+    public int numChildren;
+
+    public Vector3 Position
+    {
+        get
+        {
+            return transform.position;
+        }
+    }
+
+    public Vector3 Scale
+    {
+        get
+        {
+            Vector3 parentScale = Vector3.one;
+            if (transform.parent != null && transform.parent.GetComponent<Shape>() != null)
+            {
+                parentScale = transform.parent.GetComponent<Shape>().Scale;
+            }
+            return Vector3.Scale(transform.localScale, parentScale);
+        }
+    }
+
+    private Vector3 startpos;
+    private Vector3 goalpos;
+
+    public void Start()
+    {
+        startpos = transform.position;
+        goalpos = transform.position + movedir * moveradius;
+    }
+    public void FixedUpdate()
+    {
+        if ((transform.position - goalpos).magnitude < 0.01f)
+        {
+            movedir = -movedir;
+            goalpos = startpos;
+            startpos = transform.position;
+        }
+        Vector3 delta = movedir * movespeed * Time.deltaTime;
+        transform.position += delta;
+    }
+}
